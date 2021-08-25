772097a6df38d2048f6ec543e29db92fea0847b9
lsk-a1713313 <87055465+lsk-a1713313@users.noreply.github.com>
2021-08-25 14:00:34 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,38 +1,41 @@
+#導入Discord.py
 import discord
-import googletrans
-import os
-from pprint import pprint
-# 輸入自己Bot的TOKEN碼
-TOKEN = os.environ['TOKEN']
-SRCLanguage=os.environ['SRC']
-DSTLanguage=os.environ['DST']
-
+#client是我們與Discord連結的橋樑
 client = discord.Client()
 
-# 起動時呼叫
+#調用event函式庫
 @client.event
+#當機器人完成啟動時
 async def on_ready():
-    print('成功登入')
-
-# 收到訊息時呼叫
-@client.event
-async def on_message(message):
-    # 送信者為Bot時無視
-    if message.author.bot:
-        return
+    print('目前登入身份：',client.user)
+    game = discord.Game('努力學習python,js中')
+    #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+    await client.change_presence(status=discord.Status.idle, activity=game)
     
-    if client.user in message.mentions: # @判定
-        translator = googletrans.Translator()
-        robotName = client.user.name
-        first, space, content = message.clean_content.partition('@'+robotName+' ')
-        
-        if content == '':
-            content = first
-        if translator.detect(content).lang == DSTLanguage:
-            return
-        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest='zh-tw').text
-            await message.reply(remessage) 
+@client.event
+#當有訊息時
+async def on_message(message):
+    #排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
+        return
+    #如果以「說」開頭
+    if message.content.startswith('說'):
+      #分割訊息成兩份
+      tmp = message.content.split(" ",2)
+      #如果分割後串列長度只有1
+      if len(tmp) == 1:
+        await message.channel.send("你要我說什麼啦？")
+      else:
+        await message.channel.send(tmp[1])
+    if message.content.startswith('更改狀態'):
+        #切兩刀訊息
+        tmp = message.content.split(" ",2)
+        #如果分割後串列長度只有1
+        if len(tmp) == 1:
+            await message.channel.send("你要改成什麼啦？")
+        else:
+            game = discord.Game(tmp[1])
+            #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+            await client.change_presence(status=discord.Status.idle, activity=game)
 
-# Bot起動
-client.run(TOKEN)
+client.run('ODc5OTY2MjIyMTIzNDEzNTQ0.YSXaaA.gT6XauMQP1oyLyDUVJHqdSMpNBg') #TOKEN在剛剛Discord Developer那邊「BOT」頁面裡面            await message.reply(rem
